222002545e634e78e2e2b4e6e69cc6f5bd3debf5
tefkat1521 <84088372+tefkat1521@users.noreply.github.com>
2024-02-13 13:24:07 +0100

    d

--- code.js
+++ code.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    $("div").click(function(){
-        $("div").text("Holaaaaaaa");
+    $("#start").click(function(){
+        $("#start").css("display", "none");
+        $("#opciones").fadeIn(1000);
+        $("#opciones").css("display", "grid");
     })
+
 });
+
+
+
+// class Enemigo extends HTMLElement{
+//     constructor(){
+//         super();
+//     }
+//     connectedCallback(){
+//         this.innerHTML = "Hola ";
+//         this.style.color = "red";
+//     }
+// }
+
+// window.customElements.define("enemigo-aparecer", Enemigo);
