0a2c5e44142b7c0019a30d4312c7531fe8b7443f
tefkat1521 <84088372+tefkat1521@users.noreply.github.com>
2024-02-21 10:42:50 +0100

    d

--- codigo/partida.js
+++ codigo/partida.js
@@ -1,116 +1,146 @@
 $(document).ready(function () {
-    var puntuacion = 0; // Inicializamos la puntuación
-
-    // Escuchamos el evento de clic en la imagen
-    $("#enemigo").on("click", function () {
-        // Cambiar el cursor al cursor de disparo
-        $("#contenedorPartida").css("cursor", 'url("img/disparo.png") 16 16, auto');
-
-        // Incrementar la puntuación después de 1 milisegundo
-        setTimeout(function () {
-            puntuacion++;
-            // Actualizar la puntuación en el HTML
-            $("#puntuacion").text("Puntuación: " + puntuacion);
-
-            // Restaurar el cursor después de 1 milisegundo
-            setTimeout(function () {
-                $("#contenedorPartida").css("cursor", 'url("img/puntero.png") 16 16, auto');
-            }, 100);
-        }, 100);
-    });
-
-    function finalizar(puntuacion) {
-        alert("Tu puntuación es: " + puntuacion);
+    // Función para contar hacia atrás desde un número dado y mostrar el contador en pantalla
+    function countdown(seconds) {
+        return new Promise((resolve, reject) => {
+            let count = seconds;
+            const interval = setInterval(() => {
+                if (count > 0) {
+                    $("#contador").text(count);
+                    count--;
+                } else {
+                    $("#contador").text("¡Dispara!");
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 1000);
+        });
     }
 
-    function tal(veces) {
-        // Función para establecer la posición inicial aleatoria de la imagen
-        function setInitialPosition() {
-            var anchoAleatorio = Math.floor(Math.random() * 100) + 50;
-            var $enemigo = $("#enemigo");
-            var $contenedorPartida = $("#contenedorPartida");
-            var contenedorWidth = $contenedorPartida.width();
-            var contenedorHeight = $contenedorPartida.height();
-            $("#enemigo").width(anchoAleatorio);
-            $("#enemigo").height(anchoAleatorio);
-            var imageWidth = $enemigo.width();
-            var imageHeight = $enemigo.height();
+    // Llamada a la función countdown antes de ejecutar el código principal
+    countdown(3).then(() => {
+        // Tu código principal aquí
+        $(document).ready(function () {
+            var puntuacion = 0; // Inicializamos la puntuación
 
-            // Generar posiciones aleatorias en ambos ejes
-            var randomLeft = Math.random() * (contenedorWidth - imageWidth);
-            var randomTop = Math.random() * (contenedorHeight - imageHeight);
+            // Escuchamos el evento de clic en la imagen
+            $("#enemigo").on("click", function () {
+                // Cambiar el cursor al cursor de disparo
+                $("#contenedorPartida").css("cursor", 'url("img/disparo.png") 16 16, auto');
+                $("#contador").text(" ");
 
-            // Establecer la posición inicial aleatoria
-            $enemigo.css({
-                "left": randomLeft + "px",
-                "top": randomTop + "px"
+                // Incrementar la puntuación después de 1 milisegundo
+                setTimeout(function () {
+                    puntuacion++;
+                    // Actualizar la puntuación en el HTML
+                    $("#puntuacion").text("Puntuación: " + puntuacion);
+
+                    // Restaurar el cursor después de 1 milisegundo
+                    setTimeout(function () {
+                        $("#contenedorPartida").css("cursor", 'url("img/puntero.png") 16 16, auto');
+                    }, 100);
+                }, 100);
             });
-        }
 
-        // Función para mover la imagen fuera del contenedor
-        function moveImageOut() {
-            var $enemigo = $("#enemigo");
-            var $contenedorPartida = $("#contenedorPartida");
-            var contenedorWidth = $contenedorPartida.width();
-            var contenedorHeight = $contenedorPartida.height();
-            var imageWidth = $enemigo.width();
-            var imageHeight = $enemigo.height();
-
-            // Establecer la posición fuera del contenedor en un lado aleatorio
-            var side = Math.floor(Math.random() * 4); // 0: arriba, 1: derecha, 2: abajo, 3: izquierda
-            var left, top;
-
-            switch (side) {
-                case 0: // arriba
-                    left = Math.random() * (contenedorWidth - imageWidth);
-                    top = -imageHeight;
-                    break;
-                case 1: // derecha
-                    left = contenedorWidth;
-                    top = Math.random() * (contenedorHeight - imageHeight);
-                    break;
-                case 2: // abajo
-                    left = Math.random() * (contenedorWidth - imageWidth);
-                    top = contenedorHeight;
-                    break;
-                case 3: // izquierda
-                    left = -imageWidth;
-                    top = Math.random() * (contenedorHeight - imageHeight);
-                    break;
+            function finalizar(puntuacion) {
+                $("#enemigo").css("display", "none");
+                $("#contenedorPartida").text("Tu puntuación es: "+ puntuacion);
             }
 
-            var segundos = Math.random() * 1000 + 500;
+            function tal(veces) {
+                // Función para establecer la posición inicial aleatoria de la imagen
+                function setInitialPosition() {
+                    $("#enemigo").css("display","block");
+                    var anchoAleatorio = Math.floor(Math.random() * 100) + 50;//Tamaño aleatorio
+                    var $enemigo = $("#enemigo");
+                    var $contenedorPartida = $("#contenedorPartida");
+                    var contenedorWidth = $contenedorPartida.width();
+                    var contenedorHeight = $contenedorPartida.height();
+                    $("#enemigo").width(anchoAleatorio);
+                    $("#enemigo").height(anchoAleatorio);
+                    var imageWidth = $enemigo.width();
+                    var imageHeight = $enemigo.height();
 
-            // Animar la imagen hacia la posición calculada
-            $enemigo.animate({
-                "left": left + "px",
-                "top": top + "px"
-            }, segundos, "linear", function () {
-                // Llamar a setInitialPosition() después de completar la animación
+                    // Generar posiciones aleatorias en ambos ejes
+                    var randomLeft = Math.random() * (contenedorWidth - imageWidth);
+                    var randomTop = Math.random() * (contenedorHeight - imageHeight);
+
+                    // Establecer la posición inicial aleatoria
+                    $enemigo.css({
+                        "left": randomLeft + "px",
+                        "top": randomTop + "px"
+                    });
+                }
+
+                // Función para mover la imagen fuera del contenedor
+                function moveImageOut() {
+                    var $enemigo = $("#enemigo");
+                    var $contenedorPartida = $("#contenedorPartida");
+                    var contenedorWidth = $contenedorPartida.width();
+                    var contenedorHeight = $contenedorPartida.height();
+                    var imageWidth = $enemigo.width();
+                    var imageHeight = $enemigo.height();
+
+                    // Establecer la posición fuera del contenedor en un lado aleatorio
+                    var side = Math.floor(Math.random() * 4); // 0: arriba, 1: derecha, 2: abajo, 3: izquierda
+                    var left, top;
+
+                    switch (side) {
+                        case 0: // arriba
+                            left = Math.random() * (contenedorWidth - imageWidth);
+                            top = -imageHeight;
+                            break;
+                        case 1: // derecha
+                            left = contenedorWidth;
+                            top = Math.random() * (contenedorHeight - imageHeight);
+                            break;
+                        case 2: // abajo
+                            left = Math.random() * (contenedorWidth - imageWidth);
+                            top = contenedorHeight;
+                            break;
+                        case 3: // izquierda
+                            left = -imageWidth;
+                            top = Math.random() * (contenedorHeight - imageHeight);
+                            break;
+                    }
+
+                    var segundos = Math.random() * 1000 + 500;
+
+                    // Animar la imagen hacia la posición calculada
+                    $enemigo.animate({
+                        "left": left + "px",
+                        "top": top + "px"
+                    }, segundos, "linear", function () {
+                        // Llamar a setInitialPosition() después de completar la animación
+                        setInitialPosition();
+
+                        // Llamar a moveImageOut() nuevamente si todavía hay movimientos pendientes
+                        if (veces > 1) {
+                            veces--;
+                            moveImageOut();
+                        } else {
+                            finalizar(puntuacion);
+                        }
+                    });
+                }
+
+                // Establecer la posición inicial aleatoria de la imagen
                 setInitialPosition();
 
-                // Llamar a moveImageOut() nuevamente si todavía hay movimientos pendientes
-                if (veces > 1) {
-                    veces--;
-                    moveImageOut();
-                } else {
-                    finalizar(puntuacion);
-                }
+                // Llamar a moveImageOut() para iniciar el movimiento
+                moveImageOut();
+            }
+
+            // Llamar a tal() para que se ejecute 10 veces
+            tal(10);
+
+            // Evitar el arrastre de la imagen al mantener pulsado
+            $("#enemigo").on('mousedown', function (e) {
+                e.preventDefault();
             });
-        }
-
-        // Establecer la posición inicial aleatoria de la imagen
-        setInitialPosition();
-
-        // Llamar a moveImageOut() para iniciar el movimiento
-        moveImageOut();
-    }
-
-    // Llamar a tal() para que se ejecute 10 veces
-    tal(10);
-
-    // Evitar el arrastre de la imagen al mantener pulsado
-    $("#enemigo").on('mousedown', function (e) {
-        e.preventDefault();
+        });
+    }).catch(error => {
+        console.error('Error en el contador: ', error);
     });
+
+   
 });
